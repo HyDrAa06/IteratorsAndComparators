69a2a6c87d6a425143593221e420dddcf9b0f4a5
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-11-27 12:44:36 +0200

    Add files via upload

---
+++ Iterators.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator1
+{
+    public class Iterators<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        int index;
+
+        public List<T> Elements
+        {
+            get { return elements; }
+            set { elements = value; }
+        }
+        
+
+       
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(T element in elements) 
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public bool HasNext()
+        {
+            return index + 1 < elements.Count;
+        }
+
+        public bool Move()
+        {
+            if(index < elements.Count)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if(elements.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            Console.WriteLine(elements[index]);
+        }
+        public void PrintAll()
+        {
+            if (elements.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            List<T> temp1 = new List<T>();
+            foreach (T element in elements) 
+            {
+                temp1.Add(element);
+            }
+            Console.WriteLine(String.Join(" ", temp1));
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ListyIterator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:");
+
+            string[] create = Console.ReadLine().Split();
+            if (create.Length <= 1)
+            {
+                Console.WriteLine("Invalid Input");
+                return;
+            }
+            List<string> temp = new List<string>();
+            for(int i = 1; i < create.Length; i++)
+            {
+                temp.Add(create[i]); 
+            }
+            
+
+            Iterators<string> iterator = new Iterators<string>();
+            iterator.Elements = temp;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if(command == "Print")
+                {
+                    iterator.Print();
+                }
+                if (command == "PrintAll")
+                {
+                    iterator.PrintAll();
+                }
+                if (command == "Move")
+                {
+                    Console.WriteLine(iterator.Move());
+                }
+                if(command == "HasNext")
+                {
+                    Console.WriteLine(iterator.HasNext());
+                }
+                if(command == "END")
+                {
+                    break;  
+                }
+            }
+        }
+    }
+}
